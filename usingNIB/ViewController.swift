16f7c7d0c3fa3e277fc44a0981061e2960197caa
Mohamed Samir <mohamedsamir@Mohameds-MacBook-Air.local>
2019-09-06 05:08:22 +0200

    Initial Commit

---
+++ usingNIB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  usingNIB
+//
+//  Created by Mohamed Samir on 9/6/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
